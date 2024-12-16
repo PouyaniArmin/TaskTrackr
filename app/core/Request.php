fab18e4875c17f9aee091690a883c9c1cf736ff2
unknown <pouyaniarmin@gmail.com>
2024-12-16 11:14:49 +0330

    function path

--- app/core/Request.php
+++ app/core/Request.php
@@ -1,8 +1,16 @@
 <?php
+
 namespace App\Core;
 
-class Request{
-    public function path(){
-        
+class Request
+{
+    public function path(): string
+    {
+        $path = $_SERVER['REQUEST_URI'];
+        $position = strpos($path, "?");
+        if ($position !== false) {
+            return rtrim(htmlspecialchars(substr($path, 0, $position)), '/');
+        }
+        return rtrim(htmlspecialchars($path), '/');
     }
 }
