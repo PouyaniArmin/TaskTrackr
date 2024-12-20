c0d33a53df97328089c1e4202738d067f0ecb6c8
unknown <pouyaniarmin@gmail.com>
2024-12-20 16:04:08 +0330

    application

--- app/core/Application.php
+++ app/core/Application.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace App\Core;
 
-class Application{
-    public function run(){
-        $request=new Request;
-        echo $request->path();
+class Application
+{
+    public function run()
+    {
+        
     }
 }
