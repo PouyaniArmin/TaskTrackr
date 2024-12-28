5b3b6470b303878a98f2e0f5e527ac3b2201de93
unknown <pouyaniarmin@gmail.com>
2024-12-28 20:36:25 +0330

    add router

--- storage/webConfig.php
+++ storage/webConfig.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Controllres\HomeController;
 use App\Core\Application;
+use App\Core\Middleware\AdminMiddleware;
 use App\Core\Request;
+use App\Core\Router;
 
-$app=new Application;
+$request = new Request;
+$app = new Application(new Router($request));
+
+$app->router->get('/', [HomeController::class, 'index']);
+
+$app->router->get('/homes/{id}', [HomeController::class,'test']);
+$app->router->get('/home/{id}',function($id){
+    return "id $id";
+});
+
+
+$app->router->get('/test/post', [HomeController::class,'store'],[AdminMiddleware::class]);
+
+$app->router->get('/test',function(){
+    return "test";
+});
 $app->run();
