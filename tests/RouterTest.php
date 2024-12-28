cc18589cff7438fc7ff5791c71a8d8a53292e978
unknown <pouyaniarmin@gmail.com>
2024-12-28 20:35:57 +0330

    test

---
+++ tests/RouterTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use App\Core\Middleware\AdminMiddleware;
+use App\Core\Request;
+use App\Core\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    private Router $router;
+    public function setUp(): void
+    {
+        $this->router = new Router(new Request);
+    }
+
+    public function testCanRegisterGetRoute()
+    {
+        $_SERVER['REQUEST_URI'] = '/';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->router->get('/', function () {
+            return 'test';
+        });
+        $response = $this->router->resolve();
+        $this->assertEquals('test', $response);
+    }
+    public function testCanRegisterPostRoute()
+    {
+        $_SERVER['REQUEST_URI'] = '/';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $this->router->post('/', function () {
+            return 'test';
+        });
+        $response = $this->router->resolve();
+        $this->assertEquals('test', $response);
+    }
+    public function testRouteGetWithParameters()
+    {
+
+        $_SERVER['REQUEST_URI'] = '/new/123';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->router->get('/new/{id}', function ($id) {
+            return $id;
+        });
+        $response = $this->router->resolve();
+        $this->assertEquals('123', $response);
+    }
+    public function testRoutePostWithParameters()
+    {
+        $_SERVER['REQUEST_URI'] = '/new/123';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $this->router->post('/new/{id}', function ($id) {
+            return $id;
+        });
+        $response = $this->router->resolve();
+        $this->assertEquals('123', $response);
+    }
+
+    public function testMiddlewareIsExecutedBeforeCallback()
+    {
+        $_SERVER['REQUEST_URI'] = '/news';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $this->router->get('/news', function () {
+            return "Hi";
+        }, [AdminMiddleware::class]);
+        $response = $this->router->resolve();
+        $this->assertNoTEquals('Hi', $response);
+    }
+
+    public function testReturnsNotFoundForUnregisteredRoute()
+    {
+        $this->router->get('/posts', function () {
+            return 'test';
+        });
+        $response = $this->router->resolve();
+        $this->assertEquals('not found', $response);
+    }
+}
