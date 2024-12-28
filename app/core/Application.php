d459a3aada7af1c205ee8ce3967b69b965886206
unknown <pouyaniarmin@gmail.com>
2024-12-28 20:39:05 +0330

    update

--- app/core/Application.php
+++ app/core/Application.php
@@ -2,10 +2,29 @@
 
 namespace App\Core;
 
+use App\Core\middleware\AdminMiddleware;
+
 class Application
 {
+    public Router $router;
+    public static Application $app;
+    public Controller $controller;
+    public function __construct(Router $router)
+    {
+        self::$app = $this;
+        $this->router = $router;
+    }
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+    public function setController(Controller $controller)
+    {
+        $this->controller = $controller;
+    }
+
     public function run()
     {
-        
+        echo $this->router->resolve();
     }
 }
