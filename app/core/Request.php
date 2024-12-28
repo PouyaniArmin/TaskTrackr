9277ed957c18342fe98e3ddc2cb4a48c50965a3a
unknown <pouyaniarmin@gmail.com>
2024-12-28 20:33:28 +0330

    Implement Router and Request classes for HTTP handling

--- app/core/Request.php
+++ app/core/Request.php
@@ -4,6 +4,11 @@ namespace App\Core;
 
 class Request
 {
+    /**
+     * Get the request path without query parameters.
+     *
+     * @return string The sanitized request path.
+     */
     public function path(): string
     {
         $path = $_SERVER['REQUEST_URI'];
@@ -11,36 +16,82 @@ class Request
         if ($position !== false) {
             return rtrim(htmlspecialchars(substr($path, 0, $position)), '/');
         }
-        return rtrim(htmlspecialchars($path), '/');
+        if (preg_match('/[a-zA-Z0-9]/', $path)) {
+            return rtrim(htmlspecialchars($path), '/');
+        }
+        return htmlspecialchars($path);
     }
+    /**
+     * Get the HTTP request method (GET, POST, PUT, etc.).
+     *
+     * @return string The HTTP request method.
+     */
     public function method(): string
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
+    /**
+     * Check if the request method is GET.
+     *
+     * @return bool True if the request method is GET, otherwise false.
+     */
     public function isGet(): bool
     {
         return $this->method() === 'get';
     }
+    /**
+     * Check if the request method is POST.
+     *
+     * @return bool True if the request method is POST, otherwise false.
+     */
     public function isPost(): bool
     {
         return $this->method() === 'post';
     }
+    /**
+     * Check if the request method is PUT.
+     *
+     * @return bool True if the request method is PUT, otherwise false.
+     */
     public function isPut(): bool
     {
         return $this->method() === 'put';
     }
+    /**
+     * Check if the request method is PATCH.
+     *
+     * @return bool True if the request method is PATCH, otherwise false.
+     */
     public function isPatch(): bool
     {
         return $this->method() === 'patch';
     }
+    /**
+     * Check if the request method is DELETE.
+     *
+     * @return bool True if the request method is DELETE, otherwise false.
+     */
     public function isDelete(): bool
     {
         return $this->method() === 'delete';
     }
+    /**
+     * Check if the request method is OPTIONS.
+     *
+     * @return bool True if the request method is OPTIONS, otherwise false.
+     */
     public function isOption(): bool
     {
         return $this->method() === 'option';
     }
+    /**
+     * Get the body data of the request, sanitized for security.
+     *
+     * This method handles the input data based on the HTTP method (GET, POST, PUT, PATCH, DELETE).
+     * It returns an array of sanitized data.
+     *
+     * @return array The sanitized request body data.
+     */
     public function body(): array
     {
         $data = [];
