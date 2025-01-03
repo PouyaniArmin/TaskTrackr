3d8e8ddaa466468c0dc040219fdcea5c1ef5a80e
Pouyani Armin <pouyaniarmin@gmail.com>
2025-01-03 16:13:46 +0330

    crud db

--- app/core/Model.php
+++ app/core/Model.php
@@ -12,7 +12,18 @@ class Model extends DB
     {
         $query = "SELECT * FROM $table";
         $stmt = $this->conn->prepare($query);
+        $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $result;
+    }
+
+    public function selecBytId($table,$id): array
+    {
+        $query = "SELECT * FROM $table WHERE id=:id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(':id',intval($id),PDO::PARAM_STR);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
     public function create($table, $data)
@@ -24,7 +35,32 @@ class Model extends DB
         $query = "INSERT INTO {$table} ($columns) VALUES ($placeholders)";
         $stmt = $this->conn->prepare($query);
         foreach ($data as $key => $value) {
-            $stmt->bindParam(':' . $key, $value);
+            $stmt->bindValue(':' . $key, $value);
         }
+        $stmt->execute();
+    }
+
+    public function updateById(string $table, $id, $data)
+    {
+        $placeholders = implode(', ', array_map(function ($key) {
+            return "$key = :$key";
+        }, array_keys($data)));
+        $query = "UPDATE $table SET $placeholders WHERE id=:id;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(':id', intval($id), PDO::PARAM_INT);
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(':' . $key, $value, PDO::PARAM_STR);
+        }
+        $stmt->execute();
+        return;
+    }
+
+    public function deleteById(string $table, $id)
+    {
+        $query = "DELETE FROM $table WHERE id = :id;";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindValue(':id', intval($id), PDO::PARAM_INT);
+        $stmt->execute();
+        return;
     }
 }
