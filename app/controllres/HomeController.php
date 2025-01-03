174240d1abc2d184288676c547d9fb23f5e44a20
Pouyani Armin <pouyaniarmin@gmail.com>
2025-01-03 16:15:21 +0330

    debugging errors vaidator class

--- app/controllres/HomeController.php
+++ app/controllres/HomeController.php
@@ -4,22 +4,30 @@ namespace App\Controllres;
 
 use App\Core\Controller;
 use App\Core\Request;
+use App\Core\Validator;
 use App\Models\Users;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        $users = new Users;
-
         $data = [
             'username' => 'armin',
-            'email' => 'armin28@gmail.com',
-            'passwprd' => '1236',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')];
-        $users->insertToUsers($data);
-        // var_dump($users->getAllUsers());
+            'email' => 'armin@gmail.com',
+            'zipcode'=>'12345',
+            'password' => '12345',
+            'confrimPassword' => '12345'
+        ];
+        $fields = [
+            'username' => 'required |alphanumeric',
+            'email' => 'required | email ',
+            'zipcode'=>'required | between: 3,255',
+            'password' => 'required |secure',
+            'confrimPassword' => 'required |same:password'
+        ];
+        $validator = new Validator;
+        $errors = $validator->validation($data, $fields);
+        var_dump($errors);
         return $this->view('home');
     }
     public function test($id)
